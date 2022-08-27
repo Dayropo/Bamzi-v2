c402791fca05c6d7a67fdd189701cc851a3ca674
Adegboyega <gboyega.banjo@gmail.com>
2022-08-27 01:47:30 +0100

    layout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom"
 
 const Layout = () => {
-    return ( 
-        <main>
-            <Outlet />
-        </main>
-     );
+  return (
+    <main style={{ minHeight: "100vh" }}>
+      <Outlet />
+    </main>
+  )
 }
- 
-export default Layout;
+
+export default Layout
