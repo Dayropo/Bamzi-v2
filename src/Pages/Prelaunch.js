2ac7e8e5c2319154e76bf5efa103e11bb0de3d19
Adegboyega <gboyega.banjo@gmail.com>
2022-08-27 01:51:21 +0100

    prelaunch

--- src/Pages/Prelaunch.js
+++ src/Pages/Prelaunch.js
@@ -1,9 +1,9 @@
-import Head from '../Components/Prelaunch/Head'
-import Section1 from '../Components/Prelaunch/Section1'
-import Section2 from '../Components/Prelaunch/Section2'
-import classes from '../Styles/Prelaunch.module.css'
+import Head from "../Components/Prelaunch/Head"
+import Section1 from "../Components/Prelaunch/Section1"
+import Section2 from "../Components/Prelaunch/Section2"
+import classes from "../Styles/Prelaunch.module.css"
 
-import styled from 'styled-components'
+import styled from "styled-components"
 const StyledBody = styled.div`
 background: #E9F0FF;
 background - size: cover;
@@ -14,29 +14,32 @@ right: 0;
 bottom: 0;
 width: 100%;
 height: 100%;
-`;
+`
 
 const Prelaunch = () => {
-    return (
-        <StyledBody>
-            <div>
-                <div className={classes.container1}>
-                    <Head />
+  return (
+    <div>
+      <div className={classes.container1}>
+        <Head />
 
-                    <div className={classes.flexcont}>
-                        <div className={classes.main}>
-                            < Section1 />
-                            <Section2 />
-                        </div>
+        <div className={classes.flexcont}>
+          <div className={classes.main}>
+            <Section1 />
+            <Section2 />
+          </div>
 
-                        <div className={classes.imgast}>
-                            <img src={(require('../Asset/assets.png'))} width="350px" height="300px" alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </StyledBody>
-    )
+          <div className={classes.imgast}>
+            <img
+              src={require("../Asset/assets.png")}
+              width="350px"
+              height="300px"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Prelaunch
